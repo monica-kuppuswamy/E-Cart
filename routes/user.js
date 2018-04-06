e690a02c101287d0fff9a2a895409007a884ab90
monica-kuppuswamy <mpadmana@uncc.edu>
2018-04-05 22:13:36 -0400

    adding signup module

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,13 @@
 var router = require('express').Router();
 var User = require('../models/user');
 
+router.get('/signup', function(req, res) {
+  res.render('accounts/signup');
+});
+
 router.post('/signup', function(req, res, next) {
   var user = new User();
-  
+
   user.profile.name = req.body.name;
   user.email = req.body.email;
   user.password = req.body.password;
